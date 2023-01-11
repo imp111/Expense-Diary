10bc2fc7b9a6b6bedc4b439fc3a2e680e361225a
imp111 <alexsandartenev@gmail.com>
2023-01-11 09:09:40 +0200

    Models/Category and Transaction - added

---
+++ Expense-Diary/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Diary.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string TransactionType { get; set; } = "Expense"; // income or expense
+
+    }
+}
---
+++ Expense-Diary/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Diary.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        // Foreign Key CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public double Amount { get; set; } // amount of money spent
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Note { get; set; } // description of what the money was spent on
+
+        public DateTime date { get; set; } = DateTime.Now;
+    }
+}
