7387c52d60044e561b9f5acae9a82d60e6b18155
imp111 <alexsandartenev@gmail.com>
2023-01-11 08:43:38 +0200

    build project

---
+++ Expense-Diary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expense_Diary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
