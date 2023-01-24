5c929fb5eb1bb97f6ce628406e81a10888896711
imp111 <alexsandartenev@gmail.com>
2023-01-25 01:05:13 +0200

    succesfully display database data in the view

---
+++ Expense-Diary/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Expense_Diary.Data;
+using Expense_Diary.Models;
+
+namespace Expense_Diary.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext? _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
