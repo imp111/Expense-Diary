9c09990c1663a38e16442c2cffc90125d42949fc
imp111 <alexsandartenev@gmail.com>
2023-01-25 23:20:54 +0200

    delete items added

--- Expense-Diary/Controllers/ItemController.cs
+++ Expense-Diary/Controllers/ItemController.cs
@@ -8,20 +8,70 @@ namespace Expense_Diary.Controllers
     {
         private readonly ApplicationDbContext? _db;
 
+        // Dependency Injection
         public ItemController(ApplicationDbContext db)
         {
             _db = db;
         }
 
+        // Index Page - return all items in the database
         public IActionResult Index()
         {
             IEnumerable<Item> objList = _db.Items;
-            return View(objList);
+            return View(objList); // returns a view with all the items in the database
         }
 
+        // GET-Create Item
         public IActionResult CreateItem()
         {
             return View();
+        }
+
+        //POST-Create Item
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateItem(Item obj)
+        {
+            _db.Items.Add(obj); // adds the obj to the database
+            _db.SaveChanges(); // saves the changes
+
+            return RedirectToAction("Index"); // starts the index action
+        }
+
+        // DELETE Item GET
+        public IActionResult DeleteItem(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Items.Find(id);
+
+            if (obj== null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // DELETE Item POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteItemPost(int? id)
+        {
+            var obj = _db.Items.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Expense-Diary/Models/Item.cs
+++ Expense-Diary/Models/Item.cs
@@ -8,11 +8,13 @@ namespace Expense_Diary.Models
         [Key]
         public int Id { get; set; }
 
+        [DisplayName("Borrower:")]
         public string? Borrower { get; set; }
-        
+
+        [DisplayName("Lander:")]
         public string? Lander { get; set; }
 
-        [DisplayName("Item Name")]
+        [DisplayName("Item Name:")]
         public string? ItemName { get; set; }
     }
 }
