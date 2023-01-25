44da7afe86a0ea258d6520a2897750c8facaa57a
imp111 <alexsandartenev@gmail.com>
2023-01-25 23:28:41 +0200

    expenses view and controller added

---
+++ Expense-Diary/Controllers/ExpenseController.cs
@@ -0,0 +1,21 @@
+﻿using Expense_Diary.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Diary.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext? _db;
+
+        // Dependency Injection
+        public ExpenseController(ApplicationDbContext? db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
